c422fc26ccffdce74fb7e7f1dafb9397a89b462a
Jardel Lima <37125932+jardellimaa@users.noreply.github.com>
2018-07-17 13:39:31 -0300

    .

--- src/main/kotlin/br/unigranrio/apirestKotlin/model/Produto.kt
+++ src/main/kotlin/br/unigranrio/apirestKotlin/model/Produto.kt
@@ -8,9 +8,12 @@ data class Produto (@Id
                     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CODIGO_PRODUTO")
                     @SequenceGenerator(name = "CODIGO_PRODUTO", sequenceName = "SEQ_CODIGO_PRODUTO", allocationSize = 1)
                     val codigo: Int? = null,
+                    
                     @Column(nullable = false)
                     val nome: String = "",
+                    
                     @Column(name = "codigo_barras", nullable = false)
                     val codigoBarras: String = "",
+                    
                     @Column(nullable = false)
                     val tempo: LocalDateTime? = LocalDateTime.now())
